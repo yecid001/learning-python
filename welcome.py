2bf7b7f7d468f5ebc59265ac6ce345c5b945c6bb
yecid riascos <yecidriascos9@gmail.com>
2024-01-20 09:08:24 -0500

    add new feature; age calc

--- welcome.py
+++ welcome.py
@@ -1,5 +1,8 @@
 name = "yecid"
 country = "colombia"
+birthday= 2000
+now = 2024
 print ("you are ", name, "and you from", country)
 print("you are",name)
 print ("you are from", country)
+print ("you age is ", now-birthday)
