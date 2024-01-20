fc713e76b5631d8b6e93713f945c5edc57098bd5
yecid riascos <yecidriascos9@gmail.com>
2024-01-20 09:20:31 -0500

    upload firt commit basic_calc script: Features calc v.1.0 and v.1.1

---
+++ basic_calc.py
@@ -0,0 +1,39 @@
+#scripy tittle: basic calculator v1
+'''developer name: yecid riascos
+Date: 10-01-2024'''
+
+'''DESCRIPTION: 
+this calculator let you get the result of 4 basic  math operations
+from two numbers (variables)
+'''
+#IMPUTS (two static variables)
+num1 = 20
+num2 = 3
+
+#PROCESS
+add= num1+num2
+sub= num1 - num2
+mul= num1 * num2
+div = num1 / num2
+
+#OUTPUTS 
+print(":::BASIC CALCULATOR v1::::")
+
+print("number 1 :", num1, type(num1))
+print("number 2 :", num2, type(num2))
+
+print("the addition is:", add,type(add))
+print("the subtractio is:", sub,type(sub))
+print("the multipication is:", mul,type(mul))
+print("the dicision is:", div,type(div))
+
+print("\n")
+print(":::BASIC CALCULATOR v1.1::::")
+
+print("number 1 :", num1, type(num1))
+print("number 2 :", num2, type(num2))
+
+print("the addition is:", num1+num2)
+print("the subtractio is:", num1-num2)
+print("the multipication is:",num1*num2)
+print("the dicision is:", num1/num2)
