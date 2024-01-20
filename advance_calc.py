24c02de41206625df0993b378b6e9919f96cc383
yecid riascos <yecidriascos9@gmail.com>
2024-01-20 12:58:10 -0500

    unload advance_cal y aleatorios

---
+++ advance_calc.py
@@ -0,0 +1,13 @@
+'''crear una calculadora que reciba dos valores por consola 
+y realice las operaciones aritmeticas basicas'''
+
+import os
+os.system('clear')
+
+#imputs
+print("ingrese primer valer: ")
+n1= int (input())
+n2= int  (input("ingrese el segundo valor: \n"))
+suma=n1+n2
+
+print("suma:", suma)
---
+++ aleatorios1.py
@@ -0,0 +1,12 @@
+'''crear una calculadora que reciba dos valores  generador aleatoriamente por consola 
+y realice las operaciones aritmeticas basicas'''
+from random  import randint, uniform
+import os
+
+os.system('clear')
+
+n1= randint(-100,100)
+n2= uniform(1,45)
+
+print(n1)
+print(n2)
